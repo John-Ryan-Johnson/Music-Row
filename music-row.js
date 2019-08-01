3e4954c1c9bc6bd8fb59004bf29c3fe80cd28d96
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-08-01 13:00:15 -0500

    initial commit

---
+++ music-row.js
@@ -0,0 +1,58 @@
+const JumpStopRecords = []
+const ChattenRecords = []
+const PolarRecords = []
+
+
+const createCountryArtist = (name, genre, age) => {
+  return {
+    name: name,
+    genre: genre,
+    age: age
+  }
+
+}
+
+
+const createPopArtist = (name, genre, age) => {
+  return {
+    name: name,
+    genre: genre,
+    age: age
+  }
+}
+const createFunkArtist = (name, genre, age) => {
+  return {
+    name: name,
+    genre: genre,
+    age: age
+  }
+}
+const createRapArtist = (name, genre, age) => {
+  return {
+    name: name,
+    genre: genre,
+    age: age
+  }
+}
+const createBluegrassArtist = (name, genre, age) => {
+  return {
+    name: name,
+    genre: genre,
+    age: age
+  }
+}
+
+
+JumpStopRecords.push("Dre Funks", "Funk", 25);
+JumpStopRecords.push("Dusta Grimes", "Rap", 21);
+JumpStopRecords.push("Loyonce Branis", "Rap", 27);
+ChattenRecords.push("Bruce Atikins", "Country", 23);
+ChattenRecords.push("Bartholomew Danielson", "Bluegrass", 23);
+ChattenRecords.push("Avilee Dallas", "Country", 19);
+PolarRecords.push("Jensen Brown", "Pop", 20);
+PolarRecords.push("Austin Kinkaid", "Pop", 22);
+
+
+console.log(JumpStopRecords);
+console.log(ChattenRecords);
+console.log(PolarRecords);
